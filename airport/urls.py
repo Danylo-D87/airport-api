362887e1f73a1660c37dc347eb1b400723417906
Danylo-D87 <quaestor88@gmail.com>
2025-05-29 13:29:28 +0300

    add urls to all apps

---
+++ airport/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+# from .views import
+
+
+router = routers.DefaultRouter()
+# router.register()
+
+
+# urlpatterns = [path("", include(router.urls))]
+
+app_name = "airport"
